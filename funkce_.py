37ed44a69fbf61a830932f63f2c56f579437a61b
Jan Sukup <j.sukup@centrum.cz>
2021-11-21 14:23:51 +0100

    Initial commit

---
+++ funkce_.py
@@ -0,0 +1,72 @@
+import random
+
+def nahodne_cislo():
+    ncislo = str(random.randrange(1000,9999))
+    list_nahodne_cislo = rozdeleni(ncislo)
+    print(list_nahodne_cislo)
+    return(list_nahodne_cislo)
+
+def rozdeleni(cislo:str):
+    vysledek=[]
+    for znak in cislo:
+        vysledek.append(znak)
+    return(vysledek)
+
+def ziskani_cisla():
+    test = True
+    while test:
+        cislo=input("Zadej čtyřmístné číslo (exit pro ukončení): ")
+        if cislo == "exit":
+            quit()
+        elif len(cislo)==4 and cislo.isnumeric():
+            test = False
+        else:
+            print("Zadané číslo nesplňuje podmínku.")
+    return (cislo)
+
+def vyhodnoceni(pokus_uzivatele:list, reseni:list):
+    bb = {"bulls": 0, "cows": 0}
+    pomocne_reseni=reseni.copy()
+    for i,cislo in enumerate(pokus_uzivatele):
+        if cislo == reseni[i]:
+            bb["bulls"] += 1
+            pomocne_reseni[i]="X"
+    for i, cislo in enumerate(pokus_uzivatele):
+        if cislo in pomocne_reseni:
+            bb["cows"] += 1
+            pomocne_reseni[pomocne_reseni.index(cislo)]="X"
+#        print(cislo)
+#        print(pomocne_reseni)
+#    bb["cows"]=bb["cows"]-bb["bulls"]
+    return(bb)
+
+def sklonovani(vstupni_cislo:int):
+    if vstupni_cislo == 1:
+        sklonovani = "pokus"
+    elif vstupni_cislo > 1 and vstupni_cislo < 5:
+        sklonovani = "pokusy"
+    elif vstupni_cislo >= 5:
+        sklonovani = "pokusů"
+    return sklonovani
+
+def spojeni_listu(vstup: list):
+#    vystup=""
+#    for prvek in vstup:
+#        vystup+=str(prvek)
+#    vystup = int(''.join(list(map(lambda x: str(x), vstup))))
+    vystup=''.join(map(str, vstup))
+    return vystup
+
+def format_casu(cas: float):
+    cas=round(cas)
+    sekundy = cas % 60
+    minuty = cas // 60
+    hodiny = minuty // 60
+    minuty = minuty % 60
+    if hodiny != 0:
+        vysledny_cas = str(hodiny) + "h " + str(minuty) + "min " + str(sekundy) + "s"
+    elif hodiny == 0 and minuty != 0:
+        vysledny_cas = str(minuty) + "min " + str(sekundy) + "s"
+    elif hodiny == 0 and minuty == 0:
+        vysledny_cas = str(sekundy) + "s"
+    return(vysledny_cas)
---
+++ main.py
@@ -0,0 +1,36 @@
+from funkce_ import *
+import time
+
+oddelovac = 80*"-"
+
+print(f'''{oddelovac}
+Vítejte
+{oddelovac}
+Vygeneroval jsem pro Vás náhodné číslo.
+Pojďme hrát bulls&cows. Tak hádej, běží Vám čas!
+{oddelovac}''')
+
+#ziskani nahodneho cisla, ktere bude hadano
+reseni = nahodne_cislo()
+#pocitadlo pokusu
+pocitadlo = 0
+hadani = []
+start_time = time.time()
+while reseni != hadani:
+    #ziskani tipu od hrace
+    hadani=rozdeleni(ziskani_cisla())
+    #vyhodnoceni pokusu
+    vysledek = vyhodnoceni(hadani, reseni)
+    if hadani != reseni:
+        print("Vyhodnocení pokusu: " + str(vysledek.get("bulls")) + " bulls a "+ str(vysledek.get("cows")) + " cows.")
+    print(oddelovac)
+    pocitadlo+=1
+
+#zaverecny_tisk
+stop_time=time.time()
+time_elapsed = stop_time - start_time
+print(f"Výborně! Hledané číslo bylo {spojeni_listu(reseni)}. Uhádl jsi na {pocitadlo} {sklonovani(pocitadlo)}. Trvalo ti to {format_casu(time_elapsed)}.")
+print(oddelovac)
+
+
+
